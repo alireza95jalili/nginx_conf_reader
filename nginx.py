4bd8885907b1a8bf4a14797a2d13871e538b8032
علیرضا جلیلی <a.jalili@greenweb.local>
2020-06-14 10:48:47 +0430

    "added confreader"

--- nginx.py
+++ nginx.py
@@ -1,68 +1,46 @@
 import json
 
+
 class nginx:
     def __init__(self):
         pass
-    def conf_reader(self):
+
+    def conf_reader2(self):
         with open("nginx-config-2.txt") as f:
             lines = f.readlines()
-            
+            begining_of_server = list()
+            ending_of_server = list()
+            begining_of_upstream = list()
+            ending_of_upstream = list()
 
-            upstream_block = list()
-            
-            for line_number, line in enumerate(lines, 1):
-                # this block will search for sections
-                line = line.strip().split()
-                if "upstream" in line:
-                    if "upstream" in line[0]:
-                        strating_line = line_number
-                        
-                        for i in range(strating_line,len(lines)+1):
-                            if '}' in lines[i-1]:
-                                ending_line = i
-                                if ending_line - strating_line == 2:
-                                    # if there is only one line in section
-                                     
-                                    value = lines[ending_line - strating_line -1] + '}'
-                                    value = value.replace("\n", "")
-                                    value = value.replace("      ","")
-                                    upstream_block.append(f"{line[0]} {line[1]} {line[2]} {value}")
-                                     
-                                else:
-                                    # if there is more than one line in section
+            for n, line in enumerate(lines, 1):
+                line = line.strip()
 
-                                    value =""
-                                    block_lines = ending_line - strating_line
-                                    for i in range(strating_line,strating_line+block_lines ):
-                                        value = value + lines[i]
-                                    value = value.replace("\n", "")
-                                    value = value.replace("      ","")
-                                    upstream_block.append(f"{line[0]} {line[1]} {line[2]} {value}")
+                if line == "#begining_of_server":
+                    begining_of_server.append(n)
+                if line == "#ending_of_server":
+                    ending_of_server.append(n)
+                if line == "#begining_of_upstream":
+                    begining_of_upstream.append(n)
+                if line == "#ending_of_upstream":
+                    ending_of_upstream.append(n)
 
-                                break
+            server_blocks = list(zip(begining_of_server, ending_of_server))
+            upstream_blocks = list(zip(begining_of_upstream, ending_of_upstream))
+            del (
+                begining_of_server,
+                ending_of_server,
+                begining_of_upstream,
+                ending_of_upstream,
+            )
 
-            json_dump = json.dumps(upstream_block)
-            with  open('out.txt','w',encoding='utf-8') as myfile:
-                myfile.write(json_dump)
-                            
-
-                        
-                        
-
-
-            # for line in lines:
-            #     line = line.strip().split()
-            #     if "upstream" in line:
-            #         upstream_block[line[0]] = str(line[1]) + " {" 
-                
-            # print(upstream_block)
-
-                    
-
-
-            
-        
+            print(server_blocks)
+            print(upstream_blocks)
 
 
 test = nginx()
-test.conf_reader()
+test.conf_reader2()
+
+# serverBlock  = []
+
+# serverBlock[(0,0), (1,25)]
